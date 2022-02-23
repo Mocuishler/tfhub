ac5b658bb4389716589b769d7f4f5072e6ef9ea5
Mocuishler <anselpresent@icloud.com>
2022-02-23 16:21:37 +0800

    add tf

--- biosop/main.tf
+++ biosop/main.tf
@@ -17,12 +17,16 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "DemoVPC"
+
+  for_each = toset(["A", "B"])
+
+  name = "TF-${each.key}"
+
   cidr = "10.0.0.0/16"
 
-  azs             = []
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["cn-northwest-1a","cn-northwest-1b","cn-northwest-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
@@ -32,4 +36,3 @@ module "vpc" {
     Environment = "dev"
   }
 }
-
